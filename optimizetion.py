7d4015d0d3c04a38fb51c46e0706f83f3294133e
matankl <matankl01@gmail.com>
2025-01-15 14:10:50 +0200

    .

---
+++ optimizetion.py
@@ -0,0 +1,54 @@
+import optuna
+import train_methods as tm
+from UNet import Unet
+import os
+import torch.nn as nn
+import torch
+from tqdm import tqdm
+from const import *
+
+
+# Define the objective function for Optuna
+def objective(trial):
+    # Suggest hyperparameters
+    learning_rate = trial.suggest_loguniform('lr', 1e-5, 1e-2)
+    weight_decay = trial.suggest_loguniform('weight_decay', 1e-6, 1e-2)
+    batch_size = trial.suggest_categorical('batch_size', [2,4,8])
+
+    # Create the model
+    model = Unet(channels=CHANNELS, no_classes=NUMBER_OF_CLASSES, output_size=(640, 640)).to(DEVICE).float()
+
+    # Optimizer
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=learning_rate,
+        weight_decay=weight_decay
+    )
+
+    # Loss function
+    criterion = nn.CrossEntropyLoss()
+
+    # Data paths
+    train_image_dir = os.path.join(DATA_SET_FOLDER, train_mid_in)
+    train_label_dir = os.path.join(DATA_SET_FOLDER, train_mid_l)
+    val_image_dir = os.path.join(DATA_SET_FOLDER, val_mid_in)
+    val_label_dir = os.path.join(DATA_SET_FOLDER, val_mid_l)
+
+    # Training and validation loop
+    num_epochs = 5  # Number of epochs for each trial
+    for epoch in range(num_epochs):
+        train_loss = tm.train(model, optimizer, batch_size, train_image_dir, train_label_dir, criterion, DEVICE)
+        val_loss = tm.validate(model, batch_size, val_image_dir, val_label_dir, criterion, DEVICE)
+        print(train_loss, val_loss,)
+
+    # Return validation loss for Optuna to minimize
+    return val_loss
+
+
+# Create the study
+study = optuna.create_study(direction='minimize')
+study.optimize(objective, n_trials=20)  # Number of trials
+
+# Best parameters
+print("Best hyperparameters:", study.best_params)
+print("Best validation loss:", study.best_value)
